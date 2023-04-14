13fdf1f04ca195474dfc86113dd05126c1aec4b5
Anton <foxpain12@gmail.com>
2023-04-14 10:15:22 +0700

    check pls

--- srcs/requirements/wpc/tools/entrypoint1.sh
+++ srcs/requirements/wpc/tools/entrypoint1.sh
@@ -7,9 +7,9 @@
 
 # Set up the WordPress configuration file
 # cp /var/www/html/wordpress/wp-config.php /var/www/html/wordpress/wp-config.php
-sed -i "s/define('DB_NAME', 'database_name_here')/define('DB_NAME', '$WORDPRESS_DB_NAME')/" /var/www/html/wordpress/wp-config.php
-sed -i "s/define('DB_USER', 'username_here')/define('DB_USER', '$WORDPRESS_DB_USER')/" /var/www/html/wordpress/wp-config.php
-sed -i "s/define('DB_PASSWORD', 'password_here')/define('DB_PASSWORD', '$WORDPRESS_DB_PASSWORD')/" /var/www/html/wordpress/wp-config.php
+sed -i "s/define('DB_NAME', 'database_name_here')/define('DB_NAME', '$MYSQL_NAME')/" /var/www/html/wordpress/wp-config.php
+sed -i "s/define('DB_USER', 'username_here')/define('DB_USER', '$MYSQL_USER')/" /var/www/html/wordpress/wp-config.php
+sed -i "s/define('DB_PASSWORD', 'password_here')/define('DB_PASSWORD', '$MYSQL_PASSWORD')/" /var/www/html/wordpress/wp-config.php
 sed -i "s/define('DB_HOST', 'localhost')/define('DB_HOST', 'db')/" /var/www/html/wp-config.php
 
 # Set up Nginx configuration file
@@ -17,4 +17,4 @@ sed -i "s/server_name _;/server_name $DOMAIN_NAME;/" /etc/nginx/conf.d/default.c
 
 # Start PHP-FPM and Nginx
 php-fpm7
-nginx -g "daemon off;"
+# nginx -g "daemon off;"
